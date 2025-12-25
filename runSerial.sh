11490494f0142cfdd94909652537a6b266064841
ahmed-razic <ahmed.razic@gmail.com>
2025-12-25 23:23:47 +0100

    extracted data using foamLog and added plots

--- runSerial.sh
+++ runSerial.sh
@@ -3,6 +3,7 @@
 blockMesh | tee log.blockMesh
 checkMesh | tee log.checkMesh
 foamRun | tee log.runSerial
+foamLog log.runSerial
 foamPostProcess -func sampleDict -latestTime
 gnuplot gnuplot/gnuplotScript
-paraview cavity.foam &
+#paraview cavity.foam &
